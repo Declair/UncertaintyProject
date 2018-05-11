228706507b1365d68c3df061ac5c181e330227fa
xiahan <xh.yes@qq.com>
2018-05-11 16:55:09 +0800

    ~~~

--- src/sql.py
+++ src/sql.py
@@ -8,3 +8,5 @@ modelSql = "SELECT c_project FROM t_project"
 model_d_Sql = "SELECT arg_name FROM model_arg ORDER BY arg_id"
 
 get_model_Sql = "SELECT m.model_name, a.arg_name, a.dis_type, a.dis_arg FROM model_arg a, model m  WHERE m.model_id = a.model_id AND a.arg_name = "
+
+# get_model_Sql = "SELECT m.c_project, a.arg_name, a.dis_type, a.dis_arg FROM model_arg a, t_project m  WHERE m.n_id = a.model_id AND a.arg_name = "
